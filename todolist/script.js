796a32c223d69f4f41e945243285f3e10502f60c
Parin Jasoliya <parinjasoliya@gmail.com>
2022-01-06 14:40:15 +0530

    TodoList Working

--- todolist/script.js
+++ todolist/script.js
@@ -10,7 +10,7 @@ function addTodo(text) {
   };
 
   todoItems.push(todo);
-  console.log(todoItems);
+  renderTodos(todo);
 }
 
 const form = document.querySelector(".js-form");
@@ -25,24 +25,55 @@ form.addEventListener("submit", (event) => {
     input.value = "";
     input.focus();
   }
-
 });
 
 function renderTodos(todo) {
   const list = document.querySelector(".js-todo-list");
   const node = document.createElement("li");
 
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
   node.setAttribute("class", `todo-item `);
   node.setAttribute("data-key", todo.id);
+  let done = todo.checked ? "Completed" : "Remaining";
 
   node.innerHTML = `
    <input id="${todo.id}" type="checkbox"/>
     <label for="${todo.id}" class="tick js-tick"></label>
-    <span>${todo.text}</span>
+    <span>${todo.text} is ${done}</span>
     <button class="delete-todo js-delete-todo">
     <svg><use href="#delete-icon"></use></svg>
     </button>    <label label
     `;
 
-    list.append(node);
+  if (item) list.replaceChild(node, item);
+  else list.append(node);
+}
+
+let todolist = document.querySelector(".js-todo-list");
+
+todolist.addEventListener("click", (event) => {
+  if (event.target.classList.contains("js-tick")) {
+    const itemkey = event.target.parentElement.dataset.key;
+    toggleDone(itemkey);
+  }
+
+  if (event.target.classList.contains("delete-todo")) {
+    const itemkey = event.target.parentElement.dataset.key;
+    deleteTodo(itemkey);
+    event.target.parentElement.remove();
+  }
+});
+
+function toggleDone(key) {
+  const index = todoItems.findIndex((e) => e.id == key);
+  todoItems[index].checked = !todoItems[index].checked;
+
+  renderTodos(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  const index = todoItems.findIndex((e) => e.id === Number(key));
+  todoItems = todoItems.filter((e) => e.id !== Number(key));
+
 }
